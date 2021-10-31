32b88de8e726ad3bec8f20d66aa92d87fca60ab0
erica asgarov <ericaasgarov@gmail.com>
2021-10-31 13:08:39 +0100

    my first commit

---
+++ src/com/asgarov/Main.java
@@ -0,0 +1,19 @@
+package com.asgarov;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 15; i++) {
+            if (i%3==0){
+                System.out.println(i+"fiz");
+            }
+            if (i%5==0){
+                System.out.println(i+
+                        "buz");
+            }
+
+        }
+
+	// write your code here
+    }
+}
