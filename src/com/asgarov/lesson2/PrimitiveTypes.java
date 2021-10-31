dfa7dd9ee910f7f8a6d000a1bd4ee8c168e2d1e2
erica asgarov <ericaasgarov@gmail.com>
2021-10-31 21:02:36 +0100

    primitive types

---
+++ src/com/asgarov/lesson2/PrimitiveTypes.java
@@ -0,0 +1,17 @@
+package com.asgarov.lesson2;
+
+public class PrimitiveTypes {
+    byte a=0;
+    short b=(short)100000;
+    int c= 2_100_000_000;
+    long d= 2_222_222_222_100_000_000L;
+    float e=4.1F;
+    double f=2.2;
+    boolean w=true;
+    char character = 'm';
+
+    public static void main(String[] args) {
+        System.out.println('m'+'a');
+    }
+
+}
